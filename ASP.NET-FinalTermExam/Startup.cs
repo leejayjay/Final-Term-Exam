6021bf528991bd4aab39994b02ba869ae9e2e3ea
leejay <1103137221@gm.kuas.edu.tw>
2017-06-23 14:05:10 +0800

    加入專案檔案。

---
+++ ASP.NET-FinalTermExam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP.NET_FinalTermExam.Startup))]
+namespace ASP.NET_FinalTermExam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
